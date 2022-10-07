63f3eb53018f4683a3880ab01fcd18118c3d2e67
reneguzmanm <rene.guzman@usach.cl>
2022-10-07 00:05:59 -0300

    Path * added

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use(express.json());
 //rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.get('*', (req,res) =>{
+    res.sendFile(__dirname + '/public/index.html');
+})
 //escuchar peticiones
 
 app.listen(process.env.PORT, ()=>{
